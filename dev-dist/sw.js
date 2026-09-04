5690563a019af17f446dd42080074a49a013c28c
Foo Jin Xuan <lucagaming900@gmail.com>
2026-09-04 13:44:40 +0800

    backup before merge

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-17772b39'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "/index.html",
-    "revision": "0.jh2afr6o2lo"
+    "revision": "0.nh851n8oads"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
